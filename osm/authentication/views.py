7059253a87589339f2bd9c045911817fdca2648a
ShiQiKok <67643265+ShiQiKok@users.noreply.github.com>
2022-01-14 15:51:29 +0800

    added authentication pages

--- osm/authentication/views.py
+++ osm/authentication/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def login(request):
+    return HttpResponse("This is a login page! Welcome")
+
+def register(request):
+    return HttpResponse("This is a registration page! Welcome")
