2e9d1b29987ad67c86bb140ca2a37f2546244413
ShiQiKok <67643265+ShiQiKok@users.noreply.github.com>
2022-03-19 00:35:10 +0800

    set deault auth to login - use redirecting

--- osm/authentication/urls.py
+++ osm/authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login', views.login, name='login'),
-    path('signup', views.register, name='register'),
+    path('', views.index, name="index"), # default auth is login
+    path('login/', views.login, name='login'),
+    path('signup/', views.register, name='register'),
 ]
--- osm/authentication/views.py
+++ osm/authentication/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # redirect the index page to login
+    response  = redirect('login/')
+    return response
 
 def login(request):
     return HttpResponse("This is a login page! Welcome")
