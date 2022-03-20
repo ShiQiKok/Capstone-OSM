a14f2f7cdc46eef16825c2352f83f87af0542d7c
ShiQiKok <67643265+ShiQiKok@users.noreply.github.com>
2022-03-20 23:41:23 +0800

    add user model migrations

--- osm/user/models.py
+++ osm/user/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    # using default=uuid.uuid4 has made UUID field auto-generated
+    user_uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # TODO: Add a field for the Assessment list
