b9b7a2cf76f756e79ab19c1c264d81011f39af50
ShiQiKok <67643265+ShiQiKok@users.noreply.github.com>
2022-03-21 11:30:15 +0800

    update user views to load custom user

--- osm/user/views.py
+++ osm/user/views.py
@@ -15,17 +15,18 @@
 #         return JsonResponse(serializer.data, safe=False)
 
 
-from django.contrib.auth.models import User, Group
+# from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import UserSerializer
+from .models import User
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+    queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
