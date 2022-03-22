9f6986a30dfedaa1b7f6bc8ea32aa8e63cea2bb0
ShiQiKok <67643265+ShiQiKok@users.noreply.github.com>
2022-03-22 14:35:56 +0800

    re-applied migration & update serializer

--- osm/user/models.py
+++ osm/user/models.py
@@ -7,7 +7,6 @@ from .manager import UserManager
 class User(AbstractUser):
     # using default=uuid.uuid4 has made UUID field auto-generated
     user_uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    username = models.CharField(max_length=255, blank=False, unique=True, default="username")
     email = models.EmailField(unique=True, blank=False)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
--- osm/user/serializers.py
+++ osm/user/serializers.py
@@ -4,19 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('user_uuid', 'email', 'first_name', 'last_name', 'password', 'date_created', 'updated_at')
-
-# from django.contrib.auth.models import User, Group
-# from rest_framework import serializers
-
-
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['url', 'username', 'email', 'groups']
-
-
-# class GroupSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Group
-#         fields = ['url', 'name']
+        fields = '__all__'
