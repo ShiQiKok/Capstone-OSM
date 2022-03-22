c38eb104297d69412b54a4b1a709b56b231999e8
ShiQiKok <67643265+ShiQiKok@users.noreply.github.com>
2022-03-22 14:49:50 +0800

    deleted auth in Django backend

--- osm/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"), # default auth is login
-    path('login/', views.login, name='login'),
-    path('signup/', views.register, name='register'),
-]
