ea522ed93de41b21d5151b6ded57ac435d6fb239
ShiQiKok <67643265+ShiQiKok@users.noreply.github.com>
2022-03-24 17:52:59 +0800

    render user information

--- angular/src/app/user/user-details/user-details.component.ts
+++ angular/src/app/user/user-details/user-details.component.ts
@@ -6,6 +6,7 @@ import { AuthenticationService } from 'src/services/authentication.service';
 @Component({
     selector: 'app-user-details',
     templateUrl: './user-details.component.html',
+    styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent extends AppComponent implements OnInit {
     constructor(router: Router, _authenticationService: AuthenticationService) {
