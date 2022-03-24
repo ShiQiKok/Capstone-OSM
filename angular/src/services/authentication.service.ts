28b45d26cdfb93b70559d26e9cbad604b987dad8
ShiQiKok <67643265+ShiQiKok@users.noreply.github.com>
2022-03-24 16:21:50 +0800

    revamped front-end types

--- angular/src/services/authentication.service.ts
+++ angular/src/services/authentication.service.ts
@@ -9,7 +9,7 @@ import { User } from '../models/user';
     providedIn: 'root',
 })
 export class AuthenticationService {
-    private currentUserSubject: BehaviorSubject<any>;
+    private currentUserSubject: BehaviorSubject<User>;
     public currentUser: Observable<User>;
 
     private httpOptions: any;
@@ -30,41 +30,51 @@ export class AuthenticationService {
     }
 
     async login(username: string, password: string) {
-        this.http
-            .post<any>(
-                '/api/token/',
-                {
-                    username,
-                    password,
-                },
-                this.httpOptions
-            )
-            .subscribe(async (token) => {
-                let data = await this.updateData(token);
-                let user:User = JSON.parse(JSON.stringify(data));
-                user['token'] = token;
-                console.log("user_object");
-                console.log(user);
-                localStorage.setItem('currentUser', JSON.stringify(user));
-                this.currentUserSubject.next(user);
-            });
+        return new Promise((resolve, reject) => {
+            this.http
+                .post<any>(
+                    '/api/token/',
+                    {
+                        username,
+                        password,
+                    },
+                    this.httpOptions
+                )
+                .subscribe(
+                    async (token) => {
+                        let data = await this.updateData(token);
+                        let user: User = JSON.parse(JSON.stringify(data));
+                        user['token'] = token;
+                        console.log('user_object');
+                        console.log(user);
+                        localStorage.setItem(
+                            'currentUser',
+                            JSON.stringify(user)
+                        );
+                        this.currentUserSubject.next(user);
+                        resolve(user);
+                    },
+                    (err) => {
+                        reject(err);
+                    }
+                );
+        });
     }
 
     logout() {
         // remove user from local storage and set current user to null
         localStorage.removeItem('currentUser');
-        this.currentUserSubject.next(null);
+        this.currentUserSubject.next({} as User);
     }
 
+    // this function returns a JSON-like User object
     private updateData(token: any) {
         return new Promise((resolve, reject) => {
-            // this.token = token;
-            // this.errors = [];
-
             // decode the token to read the username and expiration timestamp
             const token_parts = token['access'].split(/\./);
             const token_decoded = JSON.parse(window.atob(token_parts[1]));
             let token_expires = new Date(token_decoded.exp * 1000);
+
             let user_id = token_decoded.user_id;
             this.http
                 .get('api/users/user-details/' + user_id)
