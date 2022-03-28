004db76e53cf8134d108bc624c3239116ea1a514
ShiQiKok <67643265+ShiQiKok@users.noreply.github.com>
2022-03-28 16:58:51 +0800

    allowed to create sub question in front end

--- angular/src/app/assessment/assessment-creation/assessment-creation-form/assessment-creation-form.component.ts
+++ angular/src/app/assessment/assessment-creation/assessment-creation-form/assessment-creation-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import {
     AbstractControl,
     FormBuilder,
@@ -13,21 +13,24 @@ import {
 })
 export class AssessmentCreationFormComponent implements OnInit {
     assessmentDetailFormGroup: FormGroup;
-    markAllocationFormGroup: FormGroup;
+    questionFormGroup: FormGroup;
     hasMarkAllocationFormBuilt: boolean = false;
 
     questionControlList: AbstractControl[] = [];
 
-    constructor(private _formBuilder: FormBuilder) {
+    constructor(
+        private _formBuilder: FormBuilder,
+        private elementRef: ElementRef
+    ) {
         this.assessmentDetailFormGroup = this._formBuilder.group({
             assessmentName: ['Quiz 1', Validators.required],
             assessmentType: ['QUESTION BASED', Validators.required],
             subject: ['CI', Validators.required],
-            noOfQuestions: ['5', Validators.required],
+            noOfQuestions: ['3', Validators.required],
         });
 
-        this.markAllocationFormGroup = this._formBuilder.group({
-            totalMark: ['', Validators.required],
+        this.questionFormGroup = this._formBuilder.group({
+            totalMark: ['100', Validators.required],
         });
     }
 
@@ -67,29 +70,59 @@ export class AssessmentCreationFormComponent implements OnInit {
             let noOfQues: number =
                 this.assessmentDetailFormGroup.get('noOfQuestions')?.value;
 
-            let element = document.getElementById('markAllocationForm');
+            let element = document.getElementById('questionForm');
 
-            for (let i = 0; i < noOfQues; i++) {
-                this.markAllocationFormGroup.addControl(
+            for (let i = 1; i <= noOfQues; i++) {
+                this.questionFormGroup.addControl(
                     `Question${i + 1}`,
-                    this._formBuilder.control('', Validators.required)
+                    this._formBuilder.control('')
                 );
 
                 if (element) {
-                    element.innerHTML += ` <div class="form-group d-flex align-items-center">
-                  <label for="Question${i + 1}">Q.${i + 1}</label>
-                  <input
-                      type="number"
-                      placeholder="Enter Mark"
-                      class="form-control ml-5"
-                      formControlName="Question${i + 1}"
-                      id="Question${i + 1}"
-                      name="Question${i + 1}"
-                  />
-              </div>`;
+                    element.appendChild(this.getQuestionTemplate(i ));
+                    let button = document.createElement('button');
+                    button.onclick = this.addSubQuestion.bind(this, i );
+                    button.innerHTML = '+ Add sub-question';
+                    button.setAttribute('class', 'btn btn-link');
+                    button.setAttribute('id', `addSubQuesButton${i }`);
+                    element.appendChild(button);
                 }
             }
             this.hasMarkAllocationFormBuilt = true;
         }
+        // TODO: update form control when the no. of ques. has changed
+    }
+
+    private getQuestionTemplate(n: number): any {
+        let group = document.createElement('div');
+        group.setAttribute('class', 'form-group');
+        group.setAttribute('id', `questionGroup${n}`);
+
+        let h5 = document.createElement('h5');
+        h5.innerHTML = `Question ${n}`;
+
+        let textarea = document.createElement('textarea');
+        textarea.setAttribute('class', 'form-control');
+        textarea.setAttribute('placeholder', `Enter question ${n}`);
+
+        group.appendChild(h5);
+        group.appendChild(textarea);
+
+        return group;
+    }
+
+    private addSubQuestion(n: number): void {
+        let container = document.getElementById(`questionGroup${n}`);
+
+        let title = document.createElement('h5');
+        title.innerHTML = `Question ${n}`;
+
+        let textarea = document.createElement('textarea');
+        textarea.setAttribute('class', 'form-control');
+        textarea.setAttribute('placeholder', `Enter question ${n}`);
+
+        container?.appendChild(title);
+        container?.appendChild(textarea);
+
     }
 }
