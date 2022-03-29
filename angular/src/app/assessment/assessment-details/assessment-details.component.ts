07234aae95d13a7f016ada99bc22db86c13e1225
ShiQiKok <67643265+ShiQiKok@users.noreply.github.com>
2022-03-29 17:39:03 +0800

    added table to assessment details page

--- angular/src/app/assessment/assessment-details/assessment-details.component.ts
+++ angular/src/app/assessment/assessment-details/assessment-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AssessmentService } from 'src/services/assessment.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Assessment } from 'src/models/assessment';
+import { MarkingSettings } from 'src/models/assessment';
+import { AnswerScriptService } from 'src/services/answer-script.service';
 
 @Component({
     selector: 'app-assessment-details',
@@ -11,22 +13,32 @@ import { Assessment } from 'src/models/assessment';
 export class AssessmentDetailsComponent implements OnInit {
     assessment!: Assessment;
 
+    isLoading: boolean = true;
+    answerScripts: any = undefined;
+
+    displayedColumns: string[] = ['studentName', 'studentId', 'lastUpdate', 'status', 'marks'];
+    markingSettings: MarkingSettings[] = Object.values(MarkingSettings);
+
     constructor(
         private _assessmentService: AssessmentService,
+        private _answerScriptService: AnswerScriptService,
         private router: Router,
         private route: ActivatedRoute
     ) {}
 
     async ngOnInit() {
+        this.isLoading = true;
         await this._assessmentService.getApi();
-        this.getAssessmentDetails();
+        await this._answerScriptService.getApi();
+        await this.getAssessmentDetails();
+        this.answerScripts = await this._answerScriptService.getAll(this.assessment.id);
+        console.log(this.answerScripts)
+        this.isLoading = false;
     }
 
-    getAssessmentDetails() {
+    async getAssessmentDetails() {
         const id = Number(this.route.snapshot.paramMap.get('id'));
-        this._assessmentService.get(id).then((assessment: any) => {
-            this.assessment = assessment;
-        });
+        this.assessment = await this._assessmentService.get(id) as Assessment;
     }
 
     updateAssessment(id: number) {
