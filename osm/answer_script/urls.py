04d7e5d11d670394d16e77cd4477058482d6385f
ShiQiKok <67643265+ShiQiKok@users.noreply.github.com>
2022-04-14 16:52:02 +0800

    implemented bulk upload script functions

--- osm/answer_script/urls.py
+++ osm/answer_script/urls.py
@@ -2,16 +2,17 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r'answerscript', views.AnswerScriptViewSet, basename="answerscript")
+# router = routers.DefaultRouter()
+# router.register(r'answerscript', views.AnswerScriptViewSet, basename="answerscript")
 
 
 urlpatterns = [
-   # path('', views.overview, name='answer-overview'),
-   path('', include(router.urls)),
+   path('', views.overview, name='answer-overview'),
+   # path('', include(router.urls)),
    path('answers/<int:assessment_id>', views.answers, name='answer-list'),
    path('answer-details/<int:id>/', views.answer_details, name='answer-details'),
    path('answer-create/', views.create_answer, name='answer-create'),
    path('answer-update/<int:id>/', views.update_answer, name='answer-update'),
    path('answer-delete/<int:id>/', views.delete_answer, name='answer-delete'),
+   path('answer-bulk-create/', views.bulk_create, name='answer-bulk-create'),
 ]
--- osm/answer_script/views.py
+++ osm/answer_script/views.py
@@ -6,9 +6,13 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .serializers import AnswerScriptSerializer
 from .models import AnswerScript
+import uuid
+import zipfile
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 # ViewSets define the view behavior.
 # To easily create a answer script in back end
+
 class AnswerScriptViewSet(ViewSet):
     serializer_class = AnswerScriptSerializer
 
@@ -31,14 +35,16 @@ class AnswerScriptViewSet(ViewSet):
 @permission_classes([IsAuthenticated])
 def overview(request):
     api_urls = {
-        'getAll':'answers/',
-        'get':'answer-details/',
-        'create':'answer-create/',
-        'update':'answer-update/',
-        'delete':'answer-delete/',
+        'getAll': 'answers/',
+        'get': 'answer-details/',
+        'create': 'answer-create/',
+        'update': 'answer-update/',
+        'delete': 'answer-delete/',
+        'bulkCreate': 'answer-bulk-create/'
     }
 
     return Response(api_urls)
+
 
 @api_view(['GET'])
 @authentication_classes([JWTAuthentication, SessionAuthentication, BasicAuthentication])
@@ -49,6 +55,7 @@ def answers(request, assessment_id):
 
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 @authentication_classes([JWTAuthentication, SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
@@ -58,7 +65,8 @@ def answer_details(request, id):
 
     return Response(serializer.data)
 
-@api_view(['GET','POST'])
+
+@api_view(['GET', 'POST'])
 @authentication_classes([JWTAuthentication, SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
 def create_answer(request):
@@ -69,6 +77,7 @@ def create_answer(request):
         return Response(serializer.data)
     else:
         return Response(serializer.errors)
+
 
 @api_view(['POST'])
 @authentication_classes([JWTAuthentication, SessionAuthentication, BasicAuthentication])
@@ -82,6 +91,7 @@ def update_answer(request, id):
 
     return Response(serializer.data)
 
+
 @api_view(['DELETE'])
 @authentication_classes([JWTAuthentication, SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
@@ -90,3 +100,55 @@ def delete_answer(request, id):
     answer.delete()
 
     return Response("AnswerScript successfully deleted")
+
+# TODO: error handling for unsupported file name
+@api_view(['POST'])
+@authentication_classes([JWTAuthentication, SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def bulk_create(request):
+    file = request.FILES['file']
+    assessment_id = request.data['assessmentId']
+    file_name = file.name
+    content_type = file.content_type
+
+    ## when this is not bulk create
+    if content_type != 'application/zip':
+        data = process_data(file_name, assessment_id, file)
+        create_request = request._request
+        create_request.POST = data
+        return create_answer(create_request)
+
+    ## when it's bulk upload file (zip)
+    else:
+        zip_file = zipfile.ZipFile(file)
+        zip_file.extractall()
+
+        # get all files except the self directory
+        for filename in zip_file.namelist()[1:]:
+            zip_ext_file = zip_file.open(filename)
+            in_memory_file = InMemoryUploadedFile(zip_ext_file, None, filename, 'application/pdf', len(zip_file.read(filename)), None)
+            filename = filename.split('/')[-1]
+            data = process_data(filename, assessment_id, in_memory_file)
+
+            create_request = request._request
+            create_request.POST = data
+            create_answer(create_request)
+
+        # close the zip_file
+        zip_file.close()
+        # remove the locally extracted files
+        remove(file)
+
+        return Response("Bulk upload successfully")
+
+def process_data(filename, assessment_id, file):
+    student_fname, student_lname, student_id, _ = filename.split('_')
+    student_id = str(uuid.uuid4())
+    return {
+        "student_name": student_fname + ' ' + student_lname,
+        "student_id": student_id,
+        "marks": None,
+        "answers": None,
+        "assessment": assessment_id,
+        "script": file
+    }
