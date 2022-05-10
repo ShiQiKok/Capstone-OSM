1018f0a9c97a3e2e82878b27292a221be39c2347
ShiQiKok <67643265+ShiQiKok@users.noreply.github.com>
2022-05-10 11:53:28 +0800

    created downloading file views in back end

--- osm/gradebook/urls.py
+++ osm/gradebook/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-#    path('<user_id>/', views.user_detail, name='user_detail'),
+    path('download/', views.download_gradebook, name='download_gradebook'),
 ]
--- osm/gradebook/views.py
+++ osm/gradebook/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from openpyxl import Workbook
+from openpyxl.writer.excel import save_virtual_workbook
+from wsgiref.util import FileWrapper
+from django.http import HttpResponse
 
-# Create your views here.
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication, SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def download_gradebook(request):
+    workbook = Workbook()
+    sheet = workbook.active
+
+    response = HttpResponse(save_virtual_workbook(workbook), content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="%s"' % 'testing.xlsx'
+
+    return response
+
+
