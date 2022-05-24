dc21426240b46ea91aae1f6c53b01672231d0433
ShiQiKok <67643265+ShiQiKok@users.noreply.github.com>
2022-05-24 21:27:41 +0800

    upload gradebook export function

--- osm/gradebook/views.py
+++ osm/gradebook/views.py
@@ -15,4 +15,27 @@ class GradebookView(PandasView):
     def filter_queryset(self, qs):
         return qs.filter(assessment_id=self.kwargs['assessment_id'])
 
+    def transform_dataframe(self, df):
+
+        marks_json_list = df.loc[:, "marks"].to_list()
+        # get the total number of collaborators for this assessment
+        collaborators_num = len(marks_json_list[0])
+
+         # exclude the "marks" column
+        df = df.loc[:, df.columns != 'marks']
+
+        marks = {}
+        for l in marks_json_list:
+            for obj in l:
+                if obj['markerId'] not in marks.keys():
+                    marks[obj['markerId']] = [obj['totalMark']]
+                else:
+                    marks[obj['markerId']].append(obj['totalMark'])
+
+
+        for marker_id, value in marks.items():
+            df[f"Marker_{marker_id}"] = value
+
+        return df
+
     renderer_classes = [PandasExcelRenderer]
